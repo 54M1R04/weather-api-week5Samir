1424065c17f15801245793fec7bea920b36411f7
54M1R04 <149613789+54M1R04@users.noreply.github.com>
2025-02-27 12:55:44 +0000

    added files in src

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
-// We will create an express app
-const app = express();
+ import weatherRoute from "./routes/weatherRoute.js";
 
-// The port that the express server will listen on
-const PORT = 3000;
-app.use(express.json());
+ const app = express();
+ const PORT = 3000;
 
-// Start the express server
-app.listen(PORT, () => {
-console.log(`Server is listening on port ${PORT}`);
-});
+ app.use(express.json());
+
+ //our new route
+ app.use("/api/weather", weatherRoute);
+
+ app.listen(PORT, () => {
+ console.log(`Server is listening on port ${PORT}`);
+ });
